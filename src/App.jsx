1439b83ac7f82ed81929f8450f534439fca46d01
gokul-saaragh <gokulchandan24@gmail.com>
2024-06-19 18:55:01 +0530

    app initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="underline">
+        Gokul
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
